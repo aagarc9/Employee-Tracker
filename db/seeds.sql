8c232208881f70ee667801adf779e353812617eb
Andrew Garcia <andrewagarcia8@gmail.com>
2022-03-20 12:37:55 -0700

    adding data to tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,4 +7,4 @@ INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Lead", 150000, 1), ("Sales Person", 80000, 1), ("Lead Engineer", 120000, 2), ("Software Engineer", 100000, 2), ("Account Manager", 100000, 3), ("Accountant", 75000, 3), ("Lawyer", 150000, 4);
 
 INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-VALUES ("Paola", "Road", 1), ("Chris", "Goldern", 2, 1), ("Andy", "Garcia", 3), ("Diego", "Jota", 4, 3), ("Mo", "Salah", 5), ("Roberto", "Firmno", 6, 5), ("Luis", "Diaz", 7);
+VALUES ("Paola", "Road", 1, NULL), ("Chris", "Goldern", 2, NULL), ("Andy", "Garcia", 3, 2), ("Diego", "Jota", 4, 3), ("Mo", "Salah", 5, NULL), ("Roberto", "Firmno", 6, 5), ("Luis", "Diaz", 7, NULL);
